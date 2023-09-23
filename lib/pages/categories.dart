ed58a6fd2ab87136a5087f9adff3e637aaef6ff5
user <username@email.com>
2023-09-23 16:42:15 +0545

    added some category

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -3,6 +3,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/widgets/app_drawer.dart';
 
+final listItems = [
+  {"type": "Clothing", "income": false},
+  {"type": "Entertainment", "income": false},
+  {"type": "Health", "income": false},
+  {"type": "Fuel", "income": false},
+  {"type": "Food", "income": false},
+  {"type": "Salary", "income": true},
+  {"type": "Bonus", "income": true},
+  {"type": "Wages", "income": true},
+];
+
 class ExpenseCategories extends StatelessWidget {
   const ExpenseCategories({super.key});
 
@@ -14,20 +25,21 @@ class ExpenseCategories extends StatelessWidget {
       ),
       drawer: const AppDrawer(),
       floatingActionButton: FloatingActionButton.extended(
+        extendedPadding: EdgeInsets.all(12),
         label: Row(
           children: [
             Padding(
               padding: const EdgeInsets.only(right: 4.0),
               child: Icon(Icons.add),
             ),
-            Text("Add ")
+            Text("Add category")
           ],
         ),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Color.fromARGB(255, 29, 89, 192),
         onPressed: () {},
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(
-            Radius.circular(8),
+            Radius.circular(30),
           ),
         ),
       ),
@@ -36,26 +48,23 @@ class ExpenseCategories extends StatelessWidget {
           const SizedBox(
             height: 10,
           ),
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.blue,
-              child: Icon(
-                Icons.add,
-                color: Colors.white,
+          for (int i = 0; i < listItems.length; i++)
+            ListTile(
+              leading: CircleAvatar(
+                backgroundColor:
+                    listItems[i]["income"] == true ? Colors.blue : Colors.red,
+                child: listItems[i]["income"] == true
+                    ? Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      )
+                    : Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
               ),
+              title: Text("${listItems[i]["type"]}"),
             ),
-            title: Text("Income"),
-          ),
-          ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.red,
-              child: Icon(
-                Icons.remove,
-                color: Colors.white,
-              ),
-            ),
-            title: Text("Expenses"),
-          ),
         ],
       ),
     );
