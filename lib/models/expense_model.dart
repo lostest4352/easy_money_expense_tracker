cd28b43dc65a49c5e56b0eea44e3c4c46f18427c
user <username@email.com>
2023-09-24 16:16:22 +0545

    commited missed file

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class ExpenseModel {
+  int year;
+  String dateTime;
+  int amount;
+  String category;
+  bool isIncome;
+
+  ExpenseModel({
+    required this.year,
+    required this.dateTime,
+    required this.amount,
+    required this.category,
+    required this.isIncome,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'year': year,
+      'dateTime': dateTime,
+      'amount': amount,
+      'category': category,
+      'isIncome': isIncome,
+    };
+  }
+
+  factory ExpenseModel.fromMap(Map<String, dynamic> map) {
+    return ExpenseModel(
+      year: map['year']?.toInt() ?? 0,
+      dateTime: map['dateTime'] ?? '',
+      amount: map['amount']?.toInt() ?? 0,
+      category: map['category'] ?? '',
+      isIncome: map['isIncome'] ?? false,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ExpenseModel.fromJson(String source) =>
+      ExpenseModel.fromMap(json.decode(source));
+}
