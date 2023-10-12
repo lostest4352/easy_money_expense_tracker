3a3a2f9e1eaa712c10e4df0cbd2da0a0603246eb
user <username@email.com>
2023-10-12 17:32:49 +0545

    minor change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -103,73 +103,76 @@ class _HomePageState extends State<HomePage> {
             height: 10,
           ),
           Flexible(
-            child: Builder(
-              builder: (context) {
-                transactionList = transactionList.where((transaction) {
-                  // TODO temp
-                  // final dtt = DateTime.parse(transaction.dateTime);
-                  // final dttformat = DateTime(dtt.year, dtt.month);
-                  // final nowdate = DateTime.now();
-                  // final nowdatef = DateTime(nowdate.year, nowdate.month -1);
-                  // return dttformat == nowdatef;
+            child: Builder(builder: (context) {
+              transactionList = transactionList.where((transaction) {
+                // TODO temp
+                // final dtt = DateTime.parse(transaction.dateTime);
+                // final dttformat = DateTime(dtt.year, dtt.month);
+                // final nowdate = DateTime.now();
+                // final nowdatef = DateTime(nowdate.year, nowdate.month -1);
+                // return dttformat == nowdatef;
 
-                  DateTimeRange firstdate = DateTimeRange(start: DateTime.parse("2023-08-01 21:29:37.782992"), end: DateTime.now(),);
-                  if (DateTime.parse(transaction.dateTime).isAfter(firstdate.start) && DateTime.parse(transaction.dateTime).isBefore(firstdate.end)) {
-                    return true;
-                  } else {
-                    return false;
-                  }
-
-                }).toList();
-                return ListView.builder(
-                  itemCount: transactionList.length,
-                  itemBuilder: (context, index) {
-                    bool isSameDate = true;
-                    String dateString = transactionList[index].dateTime;
-                    DateTime date = DateTime.parse(dateString);
-                    if (index == 0) {
-                      isSameDate = false;
-                    } else {
-                      String prevDateString = transactionList[index - 1].dateTime;
-                      DateTime prevDate = DateTime.parse(prevDateString);
-                      isSameDate = date.isSameDate(prevDate);
-                    }
-                    if (!isSameDate) {
-                      // int monthlyInc = calculateMonthsData(date).monthlyInc;
-                      // int monthlyExp = calculateMonthsData(date).monthlyExp;
-                      // int calculatedData = monthlyInc + monthlyExp;
-                      int calculatedData = calculateMonthsData(date);
-                      debugPrint("This is date: ${date.toString()}");
-                      return Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 15, right: 15, top: 4, bottom: 4),
-                            child: Row(
-                              children: [
-                                Text(date.formatDate()),
-                                const Spacer(),
-                                Text(
-                                  "Total: $calculatedData",
-                                  style: TextStyle(
-                                    color: (calculatedData > 0)
-                                        ? Colors.green
-                                        : Colors.red,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          TransactionView(transaction: transactionList[index]),
-                        ],
-                      );
-                    } else {
-                      return TransactionView(transaction: transactionList[index]);
-                    }
-                  },
+                DateTimeRange firstdate = DateTimeRange(
+                  start: DateTime.parse("2023-08-01 21:29:37.782992"),
+                  end: DateTime.now(),
                 );
-              }
-            ),
+                if (DateTime.parse(transaction.dateTime)
+                        .isAfter(firstdate.start) &&
+                    DateTime.parse(transaction.dateTime)
+                        .isBefore(firstdate.end)) {
+                  return true;
+                } else {
+                  return false;
+                }
+              }).toList();
+              return ListView.builder(
+                itemCount: transactionList.length,
+                itemBuilder: (context, index) {
+                  bool isSameDate = true;
+                  String dateString = transactionList[index].dateTime;
+                  DateTime date = DateTime.parse(dateString);
+                  if (index == 0) {
+                    isSameDate = false;
+                  } else {
+                    String prevDateString = transactionList[index - 1].dateTime;
+                    DateTime prevDate = DateTime.parse(prevDateString);
+                    isSameDate = date.isSameDate(prevDate);
+                  }
+                  if (!isSameDate) {
+                    // int monthlyInc = calculateMonthsData(date).monthlyInc;
+                    // int monthlyExp = calculateMonthsData(date).monthlyExp;
+                    // int calculatedData = monthlyInc + monthlyExp;
+                    int calculatedData = calculateMonthsData(date);
+                    debugPrint("This is date: ${date.toString()}");
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 15, right: 15, top: 4, bottom: 4),
+                          child: Row(
+                            children: [
+                              Text(date.formatDate()),
+                              const Spacer(),
+                              Text(
+                                "Total: $calculatedData",
+                                style: TextStyle(
+                                  color: (calculatedData > 0)
+                                      ? Colors.green
+                                      : Colors.red,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        TransactionView(transaction: transactionList[index]),
+                      ],
+                    );
+                  } else {
+                    return TransactionView(transaction: transactionList[index]);
+                  }
+                },
+              );
+            }),
           ),
         ],
       ),
@@ -191,71 +194,74 @@ class TransactionView extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: const Color.fromARGB(120, 33, 149, 243),
-            ),
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor:
-                          (transaction.categoryModel.isIncome == true)
-                              ? Colors.green
-                              : null,
-                      maxRadius: 12,
-                      child: (transaction.categoryModel.isIncome == true)
-                          ? const Icon(
-                              Icons.add,
-                              color: Colors.white,
-                            )
-                          : const Icon(Icons.remove),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.categoryModel.transactionType,
-                          style: const TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                        (transaction.note != null)
-                            ? Column(
-                                children: [
-                                  Text(
-                                    "${transaction.note}",
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
+          child: InkWell(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(120, 33, 149, 243),
+              ),
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor:
+                            (transaction.categoryModel.isIncome == true)
+                                ? Colors.green
+                                : null,
+                        maxRadius: 12,
+                        child: (transaction.categoryModel.isIncome == true)
+                            ? const Icon(
+                                Icons.add,
+                                color: Colors.white,
                               )
-                            : const Column(),
-                      ],
-                    ),
-                    const Spacer(),
-                    Text(
-                      DateFormat("MMM dd, EE").format(
-                        DateTime.parse(transaction.dateTime),
+                            : const Icon(Icons.remove),
                       ),
-                    ),
-                    const Spacer(),
-                    Text(
-                      "${transaction.amount}",
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.w500,
-                        color: (transaction.categoryModel.isIncome == true)
-                            ? Colors.green
-                            : Colors.red,
+                      const SizedBox(
+                        width: 8,
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transaction.categoryModel.transactionType,
+                            style: const TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          (transaction.note != null)
+                              ? Column(
+                                  children: [
+                                    Text(
+                                      "${transaction.note}",
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                )
+                              : const Column(),
+                        ],
+                      ),
+                      const Spacer(),
+                      Text(
+                        DateFormat("MMM dd, EE").format(
+                          DateTime.parse(transaction.dateTime),
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        "${transaction.amount}",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: (transaction.categoryModel.isIncome == true)
+                              ? Colors.green
+                              : Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
