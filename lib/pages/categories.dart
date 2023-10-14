949eb2d69b1a6eba8d0db85aac3adfb92c97aeb4
user <username@email.com>
2023-10-15 00:55:29 +0545

    made very basic drop down menu with items

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -14,6 +14,10 @@ class ExpenseCategories extends StatefulWidget {
 class _ExpenseCategoriesState extends State<ExpenseCategories> {
   final categoryController = TextEditingController();
 
+  bool isTrue = true;
+
+  int? colorsValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,43 +31,111 @@ class _ExpenseCategoriesState extends State<ExpenseCategories> {
           showDialog(
             context: context,
             builder: (context) {
-              return Dialog(
-                child: SizedBox(
-                  height: 400,
-                  child: Column(
-                    children: [
-                      const Spacer(),
-                      TextField(
-                        controller: categoryController,
-                        decoration: const InputDecoration(
-                          hintText: "Enter Category",
+              return StatefulBuilder(builder: (context, newState) {
+                return Dialog(
+                  child: SizedBox(
+                    height: 400,
+                    child: Column(
+                      children: [
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: categoryController,
+                            decoration: const InputDecoration(
+                              hintText: "Enter Category Name",
+                            ),
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      DropdownButton(
-                        value: false,
-                        items: const [
-                          DropdownMenuItem(value: true, child: Text("True")),
-                          DropdownMenuItem(value: false, child: Text("False")),
-                        ],
-                        onChanged: (value) {
-                          value = value;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text("Save"),
-                      ),
-                      const Spacer(),
-                    ],
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Text("Transaction Type"),
+                              const Spacer(),
+                              DropdownButton(
+                                value: isTrue,
+                                items: const [
+                                  DropdownMenuItem(
+                                    value: true,
+                                    child: Text("Income"),
+                                  ),
+                                  DropdownMenuItem(
+                                    value: false,
+                                    child: Text("Expense"),
+                                  ),
+                                ],
+                                onChanged: (value) {
+                                  newState(() {
+                                    isTrue = value ?? true;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Text("Select Color"),
+                              const Spacer(),
+                              DropdownButton(
+                                value: colorsValue,
+                                items: [
+                                  DropdownMenuItem(
+                                      value: Colors.red.value,
+                                      child: const Text("Red")),
+                                  DropdownMenuItem(
+                                      value: Colors.green.value,
+                                      child: const Text("Green")),
+                                  DropdownMenuItem(
+                                      value: Colors.blue.value,
+                                      child: const Text("Blue")),
+                                  DropdownMenuItem(
+                                      value: Colors.yellow.value,
+                                      child: const Text("Yellow")),
+                                  DropdownMenuItem(
+                                      value: Colors.purple.value,
+                                      child: const Text("Purple")),
+                                  DropdownMenuItem(
+                                      value: Colors.brown.value,
+                                      child: const Text("Brown")),
+                                  DropdownMenuItem(
+                                      value: Colors.pink.value,
+                                      child: const Text("Pink")),
+                                  DropdownMenuItem(
+                                      value: Colors.orange.value,
+                                      child: const Text("Orange")),
+                                ],
+                                onChanged: (value) {
+                                  newState(() {
+                                    colorsValue = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text("Save"),
+                        ),
+                        const Spacer(),
+                      ],
+                    ),
                   ),
-                ),
-              );
+                );
+              });
             },
           );
         },
