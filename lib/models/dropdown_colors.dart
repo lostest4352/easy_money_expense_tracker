9746f0e3e45e3981f86347699c59bd9c49ea7617
user <username@email.com>
2023-10-16 20:25:24 +0545

    made colors code consistent

---
+++ lib/models/dropdown_colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DropdownColors {
+  int colorsValue;
+  String colorsName;
+  DropdownColors({
+    required this.colorsValue,
+    required this.colorsName,
+  });
+}
+
+final List<DropdownColors> dropdownColorsList = [
+  redColor,
+  greenColor,
+  blueColor,
+  yellowColor,
+  purpleColor,
+  brownColor,
+  pinkColor,
+  orangeColor,
+];
+
+final redColor =
+    DropdownColors(colorsName: "Red", colorsValue: Colors.red.value);
+final greenColor =
+    DropdownColors(colorsName: "Green", colorsValue: Colors.green.value);
+final blueColor =
+    DropdownColors(colorsName: "Blue", colorsValue: Colors.blue.value);
+final yellowColor =
+    DropdownColors(colorsName: "Yellow", colorsValue: Colors.yellow.value);
+final purpleColor =
+    DropdownColors(colorsName: "Purple", colorsValue: Colors.purple.value);
+final brownColor =
+    DropdownColors(colorsName: "Brown", colorsValue: Colors.brown.value);
+final pinkColor =
+    DropdownColors(colorsName: "Pink", colorsValue: Colors.pink.value);
+final orangeColor =
+    DropdownColors(colorsName: "Orange", colorsValue: Colors.orange.value);
