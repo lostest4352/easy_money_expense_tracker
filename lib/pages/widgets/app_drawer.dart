65d1af266daafaa497d3a89dfb5e99e07eeadf99
user <username@email.com>
2023-10-20 02:30:32 +0545

    removed google fonts

--- lib/pages/widgets/app_drawer.dart
+++ lib/pages/widgets/app_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/routers/app_routes.dart';
 import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -13,27 +12,27 @@ class AppDrawer extends StatelessWidget {
       width: 260,
       child: ListView(
         children: [
-          DrawerHeader(
-            margin: const EdgeInsets.all(0),
-            padding: const EdgeInsets.all(0),
+          const DrawerHeader(
+            margin: EdgeInsets.all(0),
+            padding: EdgeInsets.all(0),
             child: Column(
               children: [
-                const Spacer(),
+                Spacer(),
                 Text(
                   "Easy Money",
-                  style: GoogleFonts.roboto(
-                    textStyle: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
                 ),
                 Text(
                   "Expense tracker",
-                  style: GoogleFonts.roboto(
-                    textStyle: const TextStyle(
-                        fontWeight: FontWeight.w200, fontSize: 14),
+                  style: TextStyle(
+                    fontWeight: FontWeight.w200,
+                    fontSize: 14,
                   ),
                 ),
-                const Spacer(),
+                Spacer(),
               ],
             ),
           ),
@@ -42,9 +41,8 @@ class AppDrawer extends StatelessWidget {
               context.goNamed(AppRoutes.homePage.name);
             },
             leading: const Icon(Icons.account_balance_outlined),
-            title: Text(
+            title: const Text(
               'Transactions',
-              style: GoogleFonts.roboto(),
             ),
           ),
           ListTile(
@@ -52,9 +50,8 @@ class AppDrawer extends StatelessWidget {
               context.goNamed(AppRoutes.expenseCategoriesPage.name);
             },
             leading: const Icon(Icons.label),
-            title: Text(
+            title: const Text(
               'Categories',
-              style: GoogleFonts.roboto(),
             ),
           ),
           ListTile(
@@ -62,9 +59,8 @@ class AppDrawer extends StatelessWidget {
               context.goNamed(AppRoutes.settingsPage.name);
             },
             leading: const Icon(Icons.settings),
-            title: Text(
+            title: const Text(
               'Settings',
-              style: GoogleFonts.roboto(),
             ),
           ),
         ],
