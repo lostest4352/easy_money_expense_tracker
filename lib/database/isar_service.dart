670c968c81a2231eb9dd120f3954c1ae4ee78283
user <username@email.com>
2023-10-26 20:05:33 +0545

    wrote some isar service code

--- lib/database/isar_service.dart
+++ lib/database/isar_service.dart
@@ -1,16 +1,127 @@
-// import 'package:flutter_expense_tracker/database/isar_classes.dart';
-// import 'package:isar/isar.dart';
-// import 'package:path_provider/path_provider.dart';
+import 'package:flutter_expense_tracker/database/isar_classes.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
-// class IsarInstance {
-//   Isar? isarValue;
+class IsarService {
+  late Future<Isar> isarDB;
 
-//   void getIsar() async {
-//     final dir = await getApplicationDocumentsDirectory();
-//     final isar = await Isar.open(
-//       [TransactionModelIsarSchema, CategoryModelIsarSchema],
-//       directory: dir.path,
-//     );
-//     isarValue = isar;
-//   }
-// }
+  IsarService() {
+    isarDB = openIsarDB();
+  }
+
+  // TransactionModelIsar transactionModelIsar = TransactionModelIsar();
+
+  Future<Isar> openIsarDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [TransactionModelIsarSchema, CategoryModelIsarSchema],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+    return Future.value(Isar.getInstance());
+  }
+
+  // Transaction Related
+  Future<List<TransactionModelIsar>> getTransactionData() async {
+    final isar = await isarDB;
+    final getData = await isar.transactionModelIsars.where().findAll();
+    return getData;
+  }
+
+  Stream<List<TransactionModelIsar>> listenTransactionData() async* {
+    final isar = await isarDB;
+    final listenToData =
+        isar.transactionModelIsars.where().watch(fireImmediately: true);
+    yield* listenToData;
+  }
+
+  Future<void> addTransactionData(
+      TransactionModelIsar transactionModelIsar) async {
+    final isar = await isarDB;
+    isar.writeTxn(() async {
+      isar.transactionModelIsars.put(transactionModelIsar);
+    });
+  }
+
+  Future<void> editTransactionData(int selectedTransactionModelId, int amount,
+      CategoryModelIsar categoryModelIsar, String dateTime, String note) async {
+    final isar = await isarDB;
+    final selectedUserModel =
+        await isar.transactionModelIsars.get(selectedTransactionModelId);
+
+    selectedUserModel?.amount = amount;
+    selectedUserModel?.categoryModelIsar.value = categoryModelIsar;
+    selectedUserModel?.dateTime = dateTime;
+    selectedUserModel?.note = note;
+    isar.writeTxn(() async {
+      if (selectedUserModel != null) {
+        isar.transactionModelIsars.put(selectedUserModel);
+      }
+    });
+  }
+
+  Future<void> deleteTransactionData(
+      TransactionModelIsar widgetTransactionModelIsar) async {
+    final isar = await isarDB;
+    isar.writeTxn(() async {
+      isar.transactionModelIsars.delete(widgetTransactionModelIsar.id);
+    });
+  }
+
+  // Category related
+
+  Future<List<CategoryModelIsar>> getCategoryData() async {
+    final isar = await isarDB;
+    final getData = await isar.categoryModelIsars.where().findAll();
+    return getData;
+  }
+
+  Stream<List<CategoryModelIsar>> listenCategoryData() async* {
+    final isar = await isarDB;
+    final listenToData =
+        isar.categoryModelIsars.where().watch(fireImmediately: true);
+    yield* listenToData;
+  }
+
+  Future<void> addCategoryData(CategoryModelIsar categoryModelIsars) async {
+    final isar = await isarDB;
+    isar.writeTxn(() async {
+      isar.categoryModelIsars.put(categoryModelIsars);
+    });
+  }
+
+  Future<void> editCategoryData(int selectedCategoryModelId,
+      String transactionType, bool isIncome, int colorsValue) async {
+    final isar = await isarDB;
+    final selectedCategoryModel =
+        await isar.categoryModelIsars.get(selectedCategoryModelId);
+
+    selectedCategoryModel?.transactionType = transactionType;
+
+    selectedCategoryModel?.isIncome = isIncome;
+    selectedCategoryModel?.colorsValue = colorsValue;
+    isar.writeTxn(() async {
+      if (selectedCategoryModel != null) {
+        isar.categoryModelIsars.put(selectedCategoryModel);
+      }
+    });
+  }
+
+  Future<void> deleteCategoryData(
+      TransactionModelIsar widgetTransactionModelIsar) async {
+    final isar = await isarDB;
+    isar.writeTxn(() async {
+      isar.transactionModelIsars.delete(widgetTransactionModelIsar.id);
+    });
+  }
+
+  // Delete all db items
+  Future<void> cleanDB() async {
+    final isar = await isarDB;
+    isar.writeTxn(() async {
+      isar.clear();
+    });
+  }
+}
