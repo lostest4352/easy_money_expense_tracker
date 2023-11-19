c54cd57038a29a9c54664855ef44f697b9a78b0b
user <username@email.com>
2023-11-19 11:03:00 +0545

    removed duplicate code

--- lib/pages/home_page/search_page.dart
+++ lib/pages/home_page/search_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_expense_tracker/blocs/search_cubit/search_cubit.dart';
-import 'package:flutter_expense_tracker/database/isar_service.dart';
 import 'package:flutter_expense_tracker/pages/home_page/transaction_view.dart';
 import 'package:flutter_expense_tracker/pages/widgets/popup_textfield_items.dart';
 
@@ -23,72 +22,65 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) {
-        return SearchCubit(
-          isarService: context.read<IsarService>(),
-        );
-      },
-      child: BlocBuilder<SearchCubit, SearchState>(
-        builder: (context, state) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Row(
-                children: [
-                  Expanded(
-                    child: PopupTextFieldItems(
-                      textEditingController: textEditingController,
-                      hintText: "Search",
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          return context
-                              .read<SearchCubit>()
-                              .searchClicked(textEditingController.text);
-                        },
-                        icon: const Icon(Icons.search),
-                      ),
+    return BlocBuilder<SearchCubit, SearchState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Row(
+              children: [
+                Expanded(
+                  child: PopupTextFieldItems(
+                    textEditingController: textEditingController,
+                    hintText: "Search",
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        context
+                            .read<SearchCubit>()
+                            .searchClicked(textEditingController.text);
+                      },
+                      icon: const Icon(Icons.search),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            body: BlocBuilder<SearchCubit, SearchState>(
-              builder: (context, state) {
-                if (state is SearchLoadedState) {
-                  final transactionsList = state.listOfTransactionData;
-                  return Column(
-                    children: [
-                      const SizedBox(
-                        height: 25,
+          ),
+          body: BlocBuilder<SearchCubit, SearchState>(
+            builder: (context, state) {
+              if (state is SearchLoadedState) {
+                final transactionsList = state.listOfTransactionData;
+                return Column(
+                  children: [
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: transactionsList?.length,
+                        itemBuilder: (context, index) {
+                          if (transactionsList != null) {
+                            return TransactionView(
+                              transaction: transactionsList[index],
+                            );
+                          } else {
+                            return const Center(
+                              child: Text("No Data"),
+                            );
+                          }
+                        },
                       ),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: transactionsList?.length,
-                          itemBuilder: (context, index) {
-                            if (transactionsList != null) {
-                              return TransactionView(
-                                transaction: transactionsList[index],
-                              );
-                            } else {
-                              return const Center(
-                                child: Text("No Data"),
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                    ],
-                  );
-                } else {
-                  return const Center(
-                    child: Text("Not searched yet"),
-                  );
-                }
-              },
-            ),
-          );
-        },
-      ),
+                    ),
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: Text("Not searched yet"),
+                );
+              }
+            },
+          ),
+        );
+      },
     );
   }
 }
